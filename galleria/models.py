50a48731f0b52ade0281596ba08822ed1e6a016e
grievin.okumu <grievin.okumu@student.moringaschool.com>
2021-10-09 09:29:48 +0300

    add a location model class

--- galleria/models.py
+++ galleria/models.py
@@ -7,8 +7,8 @@ class Img(models.Model):
     description = models.TextField()
     author = models.CharField(max_length=40, default='admin')
     date = models.DateTimeField(auto_now_add=True)
-    category = models.ForeignKey(Category)
-    location = models.ForeignKey(Location)
+    # category = models.ForeignKey(Category)
+    # location = models.ForeignKey(Location)
 
 class Category(models.Model):
     name = models.CharField(max_length=60)
@@ -21,3 +21,43 @@ class Category(models.Model):
 
     def delete_category(self):
         self.delete()
+
+class Location(models.Model):
+    name = models.CharField(max_length=60)
+
+    @classmethod
+    def get_locations(cls):
+        locations = Location.objects.all()
+        return locations
+
+    def __str__(self):
+        return self.name
+
+    @classmethod
+    def save_location(self):
+        self.save()
+
+    def update_location(cls, id, value):
+        cls.objects.filter(id=id).update(image=value)
+
+    def delete_locations(self):
+        self.delete()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
