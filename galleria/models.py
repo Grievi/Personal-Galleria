cbaddfea3faa3fff649dd3e2442964e2dfa9fb78
grievin.okumu <grievin.okumu@student.moringaschool.com>
2021-10-09 09:08:28 +0300

    add a class Category

--- galleria/models.py
+++ galleria/models.py
@@ -4,3 +4,20 @@ from django.db import models
 class Img(models.Model):
     img = models.ImageField(upload_to ='static/')
     name = models.CharField(max_length=60)
+    description = models.TextField()
+    author = models.CharField(max_length=40, default='admin')
+    date = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category)
+    location = models.ForeignKey(Location)
+
+class Category(models.Model):
+    name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
