a5fe51b3c54f4b0da2c5bd600f65efb2e98a1107
grievin.okumu <grievin.okumu@student.moringaschool.com>
2021-10-10 17:02:03 +0300

    register admin with models

--- galleria/admin.py
+++ galleria/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from . models import Img, Location, Category
 # Register your models here.
+admin.site.register(Img)
+admin.site.register(Location)
+admin.site.register(Category)
